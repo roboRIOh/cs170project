cdcf817d3434d18c99330f5b861cfddce41a7eb0
Rio Hayakawa <riohayakawa@berkeley.edu>
2019-11-22 14:23:54 -0800

    triangular lattice graph

--- phase1/input_generator.py
+++ phase1/input_generator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import networkx as nx
 
 # Input file format:
 # 1 Number of locations
@@ -30,19 +31,29 @@ def generate_names(locations, tas):
     # input file
     return
 
-def generate_input(size):
+def generate_input(size,x,y):
     num_of_tas = size // 2 #arbitrary number to be determined
     home_numbers = np.arange(1,num_of_tas + 1 , 1)
     list_of_locations = ["location{0}".format(i+1) for i in range(size)]
-    list_of_homes = [list_of_locations[np.random.randint(0,num_of_tas-1)] for i in range(num_of_tas)]
+    list_of_homes = []
+    while(len(list_of_homes) < num_of_tas):
+        home = list_of_locations[np.random.randint(0,num_of_tas)]
+        try:
+            list_of_homes.index("{0}".format(home))
+        except:
+            list_of_homes.append(home)
+
+    triG = nx.triangular_lattice_graph(x,y)
+    triAdj = nx.adjacency_matrix(triG).toarray()
     adjmat = np.full((size,size),'x')
     for i in range(size):
         for j in range(i):
-            x = set_edge()
-            if (x != 0):
+            x = triAdj[i,j]
+            if (triAdj[i,j] != 0):
                 adjmat[i][j] = x
                 adjmat[j][i] = x
-    file = open('{0}.out'.format(size),"w")
+    
+    file = open('{0}.in'.format(size),"w")
     file.write("{0}\n".format(size))
     file.write("{0}\n".format(num_of_tas))
     for i in range(size):
@@ -52,8 +63,8 @@ def generate_input(size):
         file.write("{0} ".format(list_of_homes[i])) 
     file.write("\n")
     file.write("{0}\n".format(list_of_locations[0]))
-    for i in range(size):
-        for j in range(size):
+    for i in range(len(adjmat)):
+        for j in range(len(adjmat[0])):
             file.write("{0} ".format(str(adjmat[i][j])))
         file.write("\n")
 
@@ -63,6 +74,11 @@ def generate_input(size):
 def set_edge():
     return np.random.randint(0,2)
 
-generate_input(50)
-generate_input(100)
-generate_input(200)
+# generate_input(10,3,3)
+generate_input(50,9,8)
+generate_input(100,19,8)
+generate_input(200,19,18)
+
+
+
+
